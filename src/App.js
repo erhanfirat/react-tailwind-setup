0d1d9f2afd306287aaf6cba8ae1a4071de537810
Erhan Fırat <erhanfirat@gmail.com>
2023-03-20 06:06:02 +0300

    app message updated

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header bg-blue container mx-3">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Tailwind is activated on CRA default react project!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
